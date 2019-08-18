2a958a25c9eb60f1ae12dc43270af5cb16ff173b
Shannon Monasco <smonasco@levelsbeyond.com>
2019-08-18 17:03:26 -0600

    some date time play

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,13 @@ repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.smonasco:swisseph:master-SNAPSHOT")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
